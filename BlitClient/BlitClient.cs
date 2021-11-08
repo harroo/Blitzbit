6f692674d561e18313840ed564a44883de227bac
harroo <harroo@protonmail.com>
2021-11-08 20:26:47 +1100

    organise client code more

--- BlitClient/BlitClient.cs
+++ BlitClient/BlitClient.cs
@@ -1,20 +1,10 @@
 
 using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Collections.Generic;
 using System.Threading;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace BlitzBit {
 
     public partial class BlitClient {
-
-        private TcpClient client;
-        private NetworkStream stream;
-
-        private bool connected;
 
         private Thread coreThread;
         private Mutex mutex = new Mutex();
@@ -25,57 +15,9 @@ namespace BlitzBit {
         }
         public BlitClient () {}
 
-        public void Connect (string address, int port) {
-
-            mutex.WaitOne(); try {
-
-                if (connected) return;
-
-                client = new TcpClient(address, port);
-                stream = client.GetStream();
-
-                coreThread = new Thread(()=>RecvLoop());
-                coreThread.Start();
-
-                connected = true;
-
-            } catch (Exception ex) {
-
-                LogError(ex.Message);
-                Crash();
-                throw;
-
-            } finally { mutex.ReleaseMutex(); }
-        }
-
         public void Close () {
 
-            mutex.WaitOne(); try {
-
-                if (!connected) return;
-
-                coreThread.Abort();
-                connected = false;
-                try {
-
-                    stream.Close();
-                    client.Close();
-
-                } catch {}
-
-            } finally { mutex.ReleaseMutex(); }
-        }
-
-        private void Crash () {
-
-            if (connected) coreThread.Abort();
-            connected = false;
-            try {
-
-                stream.Close();
-                client.Close();
-
-            } catch {}
+            Disconnect();
         }
     }
 }
---
+++ BlitClient/Connection.cs
@@ -0,0 +1,65 @@
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BlitzBit {
+
+    public partial class BlitClient {
+
+        private TcpClient client;
+        private NetworkStream stream;
+
+        private bool connected;
+
+        public void Connect (string address, int port) {
+
+            mutex.WaitOne(); try {
+
+                if (connected) return;
+
+                client = new TcpClient(address, port);
+                stream = client.GetStream();
+
+                coreThread = new Thread(()=>RecvLoop());
+                coreThread.Start();
+
+                connected = true;
+
+            } catch (Exception ex) {
+
+                LogError(ex.Message);
+
+                if (connected) coreThread.Abort();
+                connected = false;
+                try {
+
+                    stream.Close();
+                    client.Close();
+
+                } catch {}
+
+            } finally { mutex.ReleaseMutex(); }
+        }
+
+        public void Disconnect () {
+
+            mutex.WaitOne(); try {
+
+                if (!connected) return;
+
+                coreThread.Abort();
+                connected = false;
+                try {
+
+                    stream.Close();
+                    client.Close();
+
+                } catch {}
+
+            } finally { mutex.ReleaseMutex(); }
+        }
+    }
+}
