64565cfde85b6c8411a60e1662c3ebd2d03d5360
harroo <harroo@protonmail.com>
2021-11-30 16:56:12 +1100

    expand udp server functionality

--- Examples/UBlitServerExample.cs
+++ Examples/UBlitServerExample.cs
@@ -5,12 +5,42 @@ using BlitzBit;
 
 public class Test {
 
+    static UBlitServer server;
+
     public static void Main (string[] args) {
 
-        UBlitServer server = new UBlitServer("127.0.0.1", 12345);
+        server = new UBlitServer("127.0.0.1", 12345);
+
+        server.AddPacket(0, OnPacketIdZero);
+
+        server.AddPacketT(1, OnString);
 
         Console.ReadKey();
 
+        server.RelayT(1, "closing the server now, bye");
+        server.AwaitEmptySendQueue();
+
         server.Stop();
+    }
+
+    private static void OnPacketIdZero (byte[] data) {
+
+        Console.WriteLine("PacketId: 0, Length: " + data.Length + ", ");
+        Console.Write("Values: ");
+        foreach (byte b in data) {
+
+            Console.Write(b.ToString() + ", ");
+        }
+        Console.WriteLine();
+
+        server.Relay(0, data);
+    }
+
+    private static void OnString (object s) {
+
+        Console.WriteLine("String: " + (string)s);
+
+        server.RelayT(1, s + "Hello");
+        server.Relay(0, new byte[5]{8, 6, 0, 6, 4});
     }
 }
--- UBlitServer/ClientHandling.cs
+++ UBlitServer/ClientHandling.cs
@@ -1,26 +1,79 @@
 
 using System;
 using System.Net;
+using System.IO;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace BlitzBit {
 
     public partial class UBlitServer {
 
+        public void Relay (int packetId, byte[] packetData) {
+
+            byte[] sendData = new byte[packetData.Length + 2];
+
+            Buffer.BlockCopy(BitConverter.GetBytes((ushort)packetId), 0, sendData, 0, 2);
+            Buffer.BlockCopy(packetData, 0, sendData, 2, packetData.Length);
+
+            AddQueue(sendData);
+        }
+
+        public void RelayT (int packetId, object obj) {
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+
+            binaryFormatter.Serialize(memoryStream, obj);
+
+            byte[] packetData = memoryStream.ToArray();
+
+            byte[] sendData = new byte[packetData.Length + 2];
+
+            Buffer.BlockCopy(BitConverter.GetBytes((ushort)packetId), 0, sendData, 0, 2);
+            Buffer.BlockCopy(packetData, 0, sendData, 2, packetData.Length);
+
+            AddQueue(sendData);
+        }
+
         private void CoreLoop () {
+
+            bool actioned = false;
 
             while (true) {
 
-                byte[] recvData = serverClient.Receive(ref listenPoint);
+                actioned = false;
 
-                if (!clientPoints.Contains(listenPoint))
-                    clientPoints.Add(listenPoint);
+                while (serverClient.Available != 0) { actioned = true;
 
-                clientPoints.ForEach(delegate(IPEndPoint point) {
+                    byte[] recvData = serverClient.Receive(ref listenPoint);
 
-                    if (point != listenPoint)
+                    if (!clientPoints.Contains(listenPoint))
+                        clientPoints.Add(listenPoint);
 
-                    serverClient.Send(recvData, recvData.Length, point.Address.ToString(), point.Port);
-                });
+                    clientPoints.ForEach(delegate(IPEndPoint point) {
+
+                        if (point != listenPoint)
+
+                        serverClient.Send(recvData, recvData.Length, point.Address.ToString(), point.Port);
+                    });
+
+                    RelayPacket(recvData);
+                }
+
+                while (QueueHasContents()) { actioned = true;
+
+                    byte[] sendData = PopQueue();
+
+                    clientPoints.ForEach(delegate(IPEndPoint point) {
+
+                        if (point != listenPoint)
+
+                        serverClient.Send(sendData, sendData.Length, point.Address.ToString(), point.Port);
+                    });
+                }
+
+                if (!actioned) Thread.Sleep(5);
             }
         }
     }
